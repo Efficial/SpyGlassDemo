83aa1e37e0d92dfec9cc767c68d99f5a42fa0ce8
Ashley Orelien <ashleyorelien@Ashleys-MacBook-Air.local>
2022-04-15 11:52:10 -0400

    ben updates goal/milestone

--- src/main/java/fourstacks/vanguard/demo/domain/customer/model/Goal.java
+++ src/main/java/fourstacks/vanguard/demo/domain/customer/model/Goal.java
@@ -1,6 +1,42 @@
 package fourstacks.vanguard.demo.domain.customer.model;
 
-public class Goal {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
 
+@Entity
+public class Goal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String goalDescription;
+    private ArrayList<GoalMilestone> goalMilestones;
+
+    public Goal() {
+    }
+
+    public String getGoalDescription() {
+        return goalDescription;
+    }
+
+    public void setGoalDescription(String goalDescription) {
+        this.goalDescription = goalDescription;
+    }
+
+    public ArrayList<GoalMilestone> getGoalMilestones() {
+        return goalMilestones;
+    }
+
+    public void setGoalMilestones(ArrayList<GoalMilestone> goalMilestones) {
+        this.goalMilestones = goalMilestones;
+    }
+
+    @Override
+    public String toString() {
+        return "Goal{" +
+                "goalDescription='" + goalDescription + '\'' +
+                ", goalMilestones=" + goalMilestones +
+                '}';
+    }
 }
