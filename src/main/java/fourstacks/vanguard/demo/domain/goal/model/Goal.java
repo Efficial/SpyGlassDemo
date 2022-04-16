5f11601080a0cf0cc617e7873ea7f01d632e97ca
Yennifer Campos Mora <yennifercamposmora@Yennifers-Air.fios-router.home>
2022-04-16 10:42:52 -0400

    Goal class

--- src/main/java/fourstacks/vanguard/demo/domain/goal/model/Goal.java
+++ src/main/java/fourstacks/vanguard/demo/domain/goal/model/Goal.java
@@ -11,30 +11,27 @@ public class Goal {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-<<<<<<< HEAD:src/main/java/fourstacks/vanguard/demo/domain/customer/model/Goal.java
-    private GoalType goalType;
-=======
 
->>>>>>> 0b12f0c4d76fe4bb15d71c9d15a6cbd54b709c3d:src/main/java/fourstacks/vanguard/demo/domain/goal/model/Goal.java
-    private String goalDescription;
     private GoalType goalType;
+    private String goalDescription;
     private ArrayList<GoalMilestone> goalMilestones;
 
     public Goal() {
     }
 
-<<<<<<< HEAD:src/main/java/fourstacks/vanguard/demo/domain/customer/model/Goal.java
-    public Goal(String goalDescription, ArrayList<GoalMilestone> goalMilestones) {
+    public Goal(String goalDescription, ArrayList<GoalMilestone> goalMilestones, GoalType goalType) {
         this.goalDescription = goalDescription;
         this.goalMilestones = goalMilestones;
-=======
+        this.goalType = goalType;
+
+    }
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
->>>>>>> 0b12f0c4d76fe4bb15d71c9d15a6cbd54b709c3d:src/main/java/fourstacks/vanguard/demo/domain/goal/model/Goal.java
     }
 
     public GoalType getGoalType() {
@@ -65,13 +62,8 @@ public class Goal {
     public String toString() {
         return "Goal{" +
                 "id=" + id +
-<<<<<<< HEAD:src/main/java/fourstacks/vanguard/demo/domain/customer/model/Goal.java
                 ", goalType=" + goalType +
                 ", goalDescription='" + goalDescription + '\'' +
-=======
-                ", goalDescription='" + goalDescription + '\'' +
-                ", goalType=" + goalType +
->>>>>>> 0b12f0c4d76fe4bb15d71c9d15a6cbd54b709c3d:src/main/java/fourstacks/vanguard/demo/domain/goal/model/Goal.java
                 ", goalMilestones=" + goalMilestones +
                 '}';
     }
