a485cb7c75aab3c2b999b9a88eb34507a4b9e425
Ashley Orelien <ashleyorelien@Ashleys-MacBook-Air.local>
2022-04-18 08:41:53 -0400

    updated expense/goal repo, fixed merge errors

--- src/main/java/fourstacks/vanguard/demo/domain/customer/repo/CustomerRepo.java
+++ src/main/java/fourstacks/vanguard/demo/domain/customer/repo/CustomerRepo.java
@@ -6,10 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface CustomerRepo extends CrudRepository<Customer, Long> {
-<<<<<<< HEAD
+
     Optional<Customer> findByFirstAndLastName(String firstName, String lastName);
-=======
+
     Optional<Customer> findByFirstandLastName(String firstName, String lastName);
     Optional<Customer> findByUserName(String userName);
->>>>>>> 332d2ac482a7f4455a415479cd147342ae8b0623
+
 }
--- src/main/java/fourstacks/vanguard/demo/domain/expense/model/Category.java
+++ src/main/java/fourstacks/vanguard/demo/domain/expense/model/Category.java
@@ -1,5 +1,5 @@
 package fourstacks.vanguard.demo.domain.expense.model;
 
 public enum Category {
-    HOME_AUTO, EDUCATION, ENTERTAINMENT, PERSONAL_CARE, MISCELLANEOUS
+    HOME_AUTO, EDUCATION, ENTERTAINMENT, HEALTH, PERSONAL_CARE, MISCELLANEOUS
 }
